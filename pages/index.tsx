47537a75b4aa9e476fc101c82e3b6fdf1bf32fb7
Arubesu <bruno.arubesu@gmail.com>
2022-12-20 21:32:19 -0300

    chore: add raw structure html

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,28 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
+      <header>HEADER</header>
+      <main>
+        <div>
+          <input type="text" name="add-task" id="add-task" placeholder='Add a new task' />
+          <button></button>
+        </div>
+
+        <section>
+          <div>
+            <p>Created Tasks</p><span>0</span>
+          </div>
+          <div>
+            <p>Done</p><span>0</span>
+          </div>
+          <hr />
+
+          <p>ICON</p>
+          <p>You still have no tasks registered</p>
+          <p>Create tasks and organize your life</p>
+
+        </section>
+      </main>
     </>
   )
 }
